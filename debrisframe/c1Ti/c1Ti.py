3829a0182e8178d0b8abf4e42aced8264e163a74
Felix Oesterle <felix+github@alpinepics.com>
2025-02-26 10:22:32 +0100

    add Sphinx documentation configuration and initial files
    overwrite of com1DFA with still meaningless parameters
    
    first test for documentation and short description how to install and run the com1 overwrite
    
    try to adapt parameters for debris flow
    
    Update .gitignore with data files and local_ files
    
    the first reference
    
    new example data; voellmy values from literature
    
    rename module
    
    rename functions
    
    one real data example
    
    minor
    
    at first we use data from AvaFrame
    
    at first we use data from AvaFrame
    
    installation tipps in README
    
    installation tipps in README
    
    only use the changed parameters

---
+++ debrisframe/c1Ti/c1Ti.py
@@ -0,0 +1,51 @@
+"""
+    Debris flow overrides for com1DFA
+"""
+
+# Load modules
+import logging
+
+
+# Local imports
+from avaframe.in3Utils import cfgUtils
+from avaframe.in3Utils import cfgHandling
+from avaframe.com1DFA import com1DFA
+
+# create local logger
+# change log level in calling module to DEBUG to see log messages
+log = logging.getLogger(__name__)
+
+
+def c1TiMain(cfgMain, debrisCfg):
+    """Run and adjust parameters to match debris flow settings for com1DFA run,
+    result files, reports and plots are saved analog to a standard com1DFA model run
+
+    Parameters
+    -----------
+    cfgMain: configparser object
+        main DebrisFrame settings
+    debrisCfg: configparser object
+        configuration settings for debris flow including com1DFA override parameters
+
+    """
+
+    # ++++++++++ set configurations for com1DFA and override ++++++++++++
+    # get comDFA configuration and update with debris flow parameter set
+    com1DFACfg = cfgUtils.getModuleConfig(
+        com1DFA,
+        fileOverride="",
+        modInfo=False,
+        toPrint=False,
+        onlyDefault=debrisCfg["com1DFA_com1DFA_override"].getboolean("defaultConfig"),
+    )
+    com1DFACfg, debrisCfg = cfgHandling.applyCfgOverride(
+        com1DFACfg, debrisCfg, com1DFA, addModValues=False
+    )
+
+    # run the com1DFA module with debris flow settings
+    dem, plotDict, reportDictList, simDF = com1DFA.com1DFAMain(cfgMain, cfgInfo=com1DFACfg)
+
+    # print info about simulation performed to log
+    log.info("Com1DFA run performed with debris flow settings")
+
+    return dem, plotDict, reportDictList, simDF
---
+++ debrisframe/runC1Ti.py
@@ -0,0 +1,85 @@
+"""
+Run the debris flow setup of com1DFA
+"""
+
+import pathlib
+import time
+import argparse
+
+# Local imports
+# import config and init tools
+from avaframe.in3Utils import cfgUtils
+from avaframe.in3Utils import logUtils
+import avaframe.in3Utils.initializeProject as initProj
+from avaframe.in3Utils import fileHandlerUtils as fU
+
+# import computation modules
+#from debrisframe.como1Debris import como1Debris
+from c1Ti import c1Ti
+
+
+
+def runC1Ti(debrisDir=""):
+    """Run com1DFA with debris flow parameters with only an avalanche/ debris flow directory as input
+
+    Parameters
+    ----------
+    debrisDir: str
+        path to debris flow directory (setup e.g. with init scripts)
+
+    Returns
+    -------
+    peakFilesDF: pandas dataframe
+        with info about com1DFA peak file locations
+    """
+    # Time the whole routine
+    startTime = time.time()
+
+    # log file name; leave empty to use default runLog.log
+    logName = "runComo1Debris"
+
+    # Load debris flow directory from general configuration file
+    # More information about the configuration can be found here
+    # on the Configuration page in the documentation
+    cfgMain = cfgUtils.getGeneralConfig(nameFile=pathlib.Path('debrisframeCfg.ini'))
+    if debrisDir != "":
+        cfgMain["MAIN"]["avalancheDir"] = debrisDir
+        #TODO: change avalancheDir to debrisDir
+    else:
+        debrisDir = cfgMain["MAIN"]["avalancheDir"]
+
+    # Start logging
+    log = logUtils.initiateLogger(debrisDir, logName)
+    log.info("MAIN SCRIPT")
+    log.info("Current debris flow: %s", debrisDir)
+
+    # ----------------
+    # Clean input directory(ies) of old work files
+    initProj.cleanSingleAvaDir(debrisDir, deleteOutput=False)
+
+    # load debris flow config
+    DebrisCfg = cfgUtils.getModuleConfig(c1Ti)
+
+    # perform com1DFA simulation with debris flow settings
+    _, plotDict, reportDictList, _ = c1Ti.c1TiMain(cfgMain, DebrisCfg)
+
+    # Get peakfiles to return to QGIS
+    debrisDir = pathlib.Path(debrisDir)
+    inputDir = debrisDir / "Outputs" / "com1DFA" / "peakFiles"
+    peakFilesDF = fU.makeSimDF(inputDir, avaDir=debrisDir)
+
+    # Print time needed
+    endTime = time.time()
+    log.info("Took %6.1f seconds to calculate." % (endTime - startTime))
+
+    return peakFilesDF
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run debris flow workflow")
+    parser.add_argument(
+        "debrisdir", metavar="debrisdir", type=str, nargs="?", default="", help="the avalanche/ debris directory"
+    )
+    print(parser)
+    args = parser.parse_args()
+    runC1Ti(str(args.debrisdir))
