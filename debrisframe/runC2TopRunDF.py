e133219b3a3b370df743abb39f1c23e987f5d533
paulasp <paula.spannring@gmail.com>
2026-01-22 16:25:27 +0100

    add original structure of TopRunDF, (to run only one scenario)
    add computation
    
    tidy up the code
    
    tidy up
    
    correct logger

---
+++ debrisframe/runC2TopRunDF.py
@@ -0,0 +1,55 @@
+import pathlib
+import time
+import argparse
+
+# Local imports
+# import config and init tools
+from avaframe.in3Utils import cfgUtils
+from avaframe.in3Utils import logUtils
+import avaframe.in3Utils.initializeProject as initProj
+
+from c2TopRunDF import c2TopRunDF
+
+def runC2TopRunDF(debrisDir=""):
+
+    # Time the whole routine
+    startTime = time.time()
+
+    # log file name; leave empty to use default runLog.log
+    logName = "runC2TopRunDF"
+
+    # Load debris flow directory from general configuration file
+    # More information about the configuration can be found here
+    # on the Configuration page in the documentation
+    cfgMain = cfgUtils.getGeneralConfig(nameFile=pathlib.Path("debrisframeCfg.ini"))
+    if debrisDir != "":
+        cfgMain["MAIN"]["avalancheDir"] = debrisDir
+        # TODO: change avalancheDir to debrisDir
+    else:
+        debrisDir = cfgMain["MAIN"]["avalancheDir"]
+
+    # Start logging
+    log = logUtils.initiateLogger(debrisDir, logName)
+    log.info("MAIN SCRIPT")
+    log.info("Current debris flow: %s", debrisDir)
+
+    initProj.cleanSingleAvaDir(debrisDir, deleteOutput=False)
+    DebrisCfg = cfgUtils.getModuleConfig(c2TopRunDF)
+    c2TopRunDF.c2TopRunDFMain(cfgMain, DebrisCfg)
+
+    endTime = time.time()
+    log.info("Took %6.1f seconds to calculate." % (endTime - startTime))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run debris flow workflow")
+    parser.add_argument(
+        "debrisdir",
+        metavar="debrisdir",
+        type=str,
+        nargs="?",
+        default="",
+        help="the debris directory",
+    )
+    args = parser.parse_args()
+    runC2TopRunDF(str(args.debrisdir))
