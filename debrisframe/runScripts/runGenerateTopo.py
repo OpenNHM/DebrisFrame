09b85143b827a01903fb70e185df4865ed1f8cd9
JuLa96 <206707905+JuLa96@users.noreply.github.com>
2026-02-12 12:14:57 +0100

    Update runScripts/runGenerateTopo.py

--- debrisframe/runScripts/runGenerateTopo.py
+++ debrisframe/runScripts/runGenerateTopo.py
@@ -2,7 +2,6 @@
 """
     Run script for generateTopo in module in3Utils
 """
-import logging
 import pathlib
 
 # Local imports
@@ -10,24 +9,23 @@ from debrisframe.in1Utils import generateTopo as gT
 from debrisframe.out1Plot import outTopo as oT
 from avaframe.in3Utils import cfgUtils, logUtils
 
+if __name__ == '__main__':
+    # log file name; leave empty to use default runLog.log
+    logName = 'generateTopo'
 
-# log file name; leave empty to use default runLog.log
-logName = 'generateTopo'
+    # Load avalanche directory from general configuration file
+    cfgMain = cfgUtils.getGeneralConfig(nameFile=pathlib.Path("debrisframeCfg.ini"))
+    debrisDir = cfgMain["MAIN"]["avalancheDir"]
 
-# Load avalanche directory from general configuration file
-cfgMain = cfgUtils.getGeneralConfig(nameFile=pathlib.Path("debrisframeCfg.ini"))
-debrisDir = cfgMain["MAIN"]["avalancheDir"]
-# avalancheDir = cfgMain['MAIN']['avalancheDir']
+    # Start logging
+    log = logUtils.initiateLogger(debrisDir, logName)
+    log.info('MAIN SCRIPT')
 
-# Start logging
-log = logUtils.initiateLogger(debrisDir, logName)
-log.info('MAIN SCRIPT')
+    # Load input parameters from configuration file
+    cfg = cfgUtils.getModuleConfig(gT, debrisDir)
 
-# Load input parameters from configuration file
-cfg = cfgUtils.getModuleConfig(gT, debrisDir)
+    # Call main function to generate DEMs
+    [z, name_ext, outDir] = gT.generateTopo(cfg, debrisDir)
 
-# Call main function to generate DEMs
-[z, name_ext, outDir] = gT.generateTopo(cfg, debrisDir)
-
-# Plot new topogrpahy
-oT.plotGeneratedDEM(z, name_ext, cfg, outDir, cfgMain)
+    # Plot new topogrpahy
+    oT.plotGeneratedDEM(z, name_ext, cfg, outDir, cfgMain)
