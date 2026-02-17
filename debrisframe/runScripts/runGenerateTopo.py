5323e3affd8e1ba01b12fc6e4ad1efb057aaa20c
JuLa96 <206707905+JuLa96@users.noreply.github.com>
2026-02-17 10:44:25 +0100

    Update runScripts/runGenerateTopo.py

--- debrisframe/runScripts/runGenerateTopo.py
+++ debrisframe/runScripts/runGenerateTopo.py
@@ -7,7 +7,7 @@ import pathlib
 # Local imports
 from debrisframe.in1Utils import generateTopo as gT
 from avaframe.out3Plot import outTopo as oT
-from avaframe.in3Utils import cfgUtils, logUtils
+from avaframe.in3Utils import cfgUtils, logUtils, generateTopo, cfgHandling
 
 if __name__ == '__main__':
     # log file name; leave empty to use default runLog.log
@@ -21,8 +21,10 @@ if __name__ == '__main__':
     log = logUtils.initiateLogger(debrisDir, logName)
     log.info('MAIN SCRIPT')
 
-    # Load input parameters from configuration file
-    cfg = cfgUtils.getModuleConfig(gT, debrisDir)
+    # Load default input parameters from configuration file and update with override parameters
+    debrisCfg = cfgUtils.getModuleConfig(gT)
+    cfg = cfgUtils.getModuleConfig(generateTopo,onlyDefault=debrisCfg["in3Utils_generateTopo_override"].getboolean("defaultConfig"))
+    cfg, debrisCfg = cfgHandling.applyCfgOverride(cfg, debrisCfg, generateTopo, addModValues=False)
 
     # Call main function to generate DEMs
     [z, name_ext, outDir] = gT.generateTopo(cfg, debrisDir)
